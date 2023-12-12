8545eb26d500b8463d425ac9b01fb60fff240cd7
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2023-12-12 10:58:54 -0600

    Added index page for foodle app

--- foodle/views.py
+++ foodle/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the foodle index.")
+
